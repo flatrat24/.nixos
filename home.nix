c717287df6a6499f14eca312f783251c1b62e185
Ethan Anthony <ethan.anthony@du.edu>
2024-11-30 04:52:08 -0500

    add mime.nix and organize modules

--- home.nix
+++ home.nix
@@ -2,11 +2,20 @@
 
 {
   imports = [
+    # General Modules
+    ./config/home/fonts.nix
+    ./config/home/mime.nix
+
+    # Shell/Terminal Modules
     ./config/home/sh.nix
     ./config/home/yazi.nix
     ./config/home/zellij.nix
-    ./config/home/fonts.nix
     ./config/home/git.nix
+    ./config/home/nvim.nix
+
+    # Application Modules
+
+    # Other Modules
     # ./config/home/mpd.nix
     ./config/home/hypr.nix
   ];
