79b6986ba8157a2b238c6c34b9ae38252a2b67fd
Ethan Anthony <ethan.anthony@du.edu>
2024-11-30 19:16:30 -0500

    adding stylix tentatively

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    stylix.url = "github:danth/stylix";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
@@ -14,32 +15,35 @@
       leo = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-	  ./config/system/basic-packages.nix
-	  ./config/system/network.nix
-	  # ./config/system/nvidia.nix
-	  ./config/system/nvidia-hyprland.nix
-	  ./config/system/bluetooth.nix
-	  ./config/system/grub.nix
-	  # ./config/system/sddm.nix
+          ./config/system/basic-packages.nix
+          ./config/system/network.nix
+          # ./config/system/nvidia.nix
+          ./config/system/nvidia-hyprland.nix
+          ./config/system/bluetooth.nix
+          ./config/system/grub.nix
+          # ./config/system/sddm.nix
           ./hosts/loque/configuration.nix
 
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.ea = import ./home.nix;
-	  }
+          }
+
+          inputs.stylix.nixosModules.stylix
         ];
       };
       leoito = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-	  ./config/system/basic-packages.nix
-	  ./config/system/network.nix
-	  # ./config/system/nvidia.nix
-	  # ./config/system/nvidia-hyprland.nix
-	  ./config/system/bluetooth.nix
-	  ./config/system/grub.nix
-	  # ./config/system/sddm.nix
+          ./config/system/basic-packages.nix
+          ./config/system/stylix.nix
+          ./config/system/network.nix
+          # ./config/system/nvidia.nix
+          # ./config/system/nvidia-hyprland.nix
+          ./config/system/bluetooth.nix
+          ./config/system/grub.nix
+          # ./config/system/sddm.nix
           ./hosts/frame/configuration.nix
 
           home-manager.nixosModules.home-manager {
