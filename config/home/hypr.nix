1a9f4ef27e7af058f814504ca69382cae8403795
Ethan Anthony <ethan.anthony@du.edu>
2024-11-30 19:16:44 -0500

    tentative hypr config

--- config/home/hypr.nix
+++ config/home/hypr.nix
@@ -36,19 +36,138 @@
 
   #### HYPRLAND TRANSFORMATION ####
   # Main Settings
+  programs.kitty.enable = true;
   wayland.windowManager.hyprland.enable = true;
 
   # Theme
   wayland.windowManager.hyprland.settings = {
+    # COLORS #
+    "$cursor" = "rgba(928374ee)";
+
+    "$url_color" = "rgba(83a598ee)";
+
+    "$visual_bell_color" = "rgba(8ec07cee)";
+    "$bell_border_color" = "rgba(8ec07cee)";
+
+    "$active_border_color" = "rgba(d3869bee)";
+    "$inactive_border_color" = "rgba(665c54ee)";
+
+    "$foreground" = "rgba(ebdbb2ee)";
+    "$background" = "rgba(282828ee)";
+    "$selection_foreground" = "rgba(928374ee)";
+    "$selection_background" = "rgba(ebdbb2ee)";
+
+    "$active_tab_foreground" = "rgba(fbf1c7ee)";
+    "$active_tab_background" = "rgba(665c54ee)";
+    "$inactive_tab_foreground" = "rgba(a89984ee)";
+    "$inactive_tab_background" = "rgba(3c3836ee)";
+
+    # black  (bg3/bg4)
+    "$color0" = "rgba(665c54ee)";
+    "$color8" = "rgba(7c6f64ee)";
+
+    # red
+    "$color1" = "rgba(cc241dee)";
+    "$color9" = "rgba(fb4934ee)";
+
+    # green
+    "$color2" = "rgba(98971aee)";
+    "$color10" = "rgba(b8bb26ee)";
+
+    # yellow
+    "$color3" = "rgba(d79921ee)";
+    "$color11" = "rgba(fabd2fee)";
+
+    # blue
+    "$color4" = "rgba(458588ee)";
+    "$color12" = "rgba(83a598ee)";
+
+    # purple
+    "$color5" = "rgba(b16286ee)";
+    "$color13" = "rgba(d3869bee)";
+
+    # aqua
+    "$color6" = "rgba(689d6aee)";
+    "$color14" = "rgba(8ec07cee)";
+
+    # white (fg4/fg3)
+    "$color7" = "rgba(a89984ee)";
+    "$color15" = "rgba(bdae93ee)";
+
+    "$mod" = "SUPER";
+    "$terminal" = "foot";
+    bindd = [
+      "$mod, Escape, Lock Screen, exec, hyprlock"
+      "$mod, A, Launch Terminal, exec, $terminal"
+    ];
+
     general = {
-      gaps_in = "5";
-      gaps_out = "5";
-      border_size = "3";
-      col.active_border = "$color11";
-      col.inactive_border = "$color2";
-      resize_on_border = "true" ;
-      allow_tearing = "false";
-      layout = "master";
+      "gaps_in" = "5";
+      "gaps_out" = "5";
+      "border_size" = "3";
+      "col.active_border" = "$color11";
+      "col.inactive_border" = "$color2";
+      "resize_on_border" = "true" ;
+      "allow_tearing" = "false";
+      "layout" = "master";
+    };
+
+    decoration = {
+      "rounding" = "0";
+      "active_opacity" = "1.0";
+      "inactive_opacity" = "1.0";
+      "drop_shadow" = "true";
+      "shadow_range" = "4";
+      "shadow_render_power" = "3";
+      "col.shadow" = "rgba(1a1a1aee)";
+      "blur" = {
+        "enabled" = "true";
+        "size" = "3";
+        "passes" = "1";
+        "vibrancy" = "0.1696";
+      };
+    };
+
+    # animations = {
+    #   "enabled" = "true";
+    #   "bezier" = "myBezier, 0.05, 0.9, 0.1, 1.05";
+    #   "animation" = "windows, 1, 2, myBezier, slidein";
+    #   "animation" = "border, 1, 2, default";
+    #   "animation" = "fade, 1, 2, default";
+    #   "animation" = "workspaces, 1, 2, default, slide";
+    # };
+
+    dwindle = {
+      "pseudotile" = "true";
+      "preserve_split" = "true";
+    };
+
+    master = {
+      "new_status" = "slave";
+    };
+
+    misc = {
+      "force_default_wallpaper" = "-1";
+      "disable_hyprland_logo" = "true";
+    };
+
+    group = {
+      "col.border_active" = "$color11";
+      "col.border_inactive" = "$color2";
+      "col.border_locked_active" = "$color11";
+      "col.border_locked_inactive" = "$color2";
+      groupbar = {
+        "enabled" = "true";
+        "font_size" = "10";
+        "font_family" = "Fira Code Nerd Font Mono";
+        "height" = "14";
+        "render_titles" = "true";
+        "text_color" = "$background";
+        "col.active" = "$color10";
+        "col.inactive" = "$color2";
+        "col.locked_active" = "$color10";
+        "col.locked_inactive" = "$color2";
+      };
     };
   };
 
