650d49f561cbd8536d4502a0bf41d5bbd7bf2030
Ethan Anthony <ethan.anthony@du.edu>
2024-12-02 18:35:04 -0500

    removed undesired home directories

--- home.nix
+++ home.nix
@@ -31,9 +31,14 @@
   xdg.userDirs = {
     enable = true;
     createDirectories = true;
+    desktop = null; # "${config.home.homeDirectory}/Desktop";
     documents = "${config.home.homeDirectory}/Documents";
     download = "${config.home.homeDirectory}/Downloads";
     music = "${config.home.homeDirectory}/Music";
+    pictures = null; # "${config.home.homeDirectory}/Pictures";
+    publicShare = null; # "${config.home.homeDirectory}/Public";
+    templates = null; # "${config.home.homeDirectory}/Templates";
+    videos = null; # "${config.home.homeDirectory}/Videos";
   };
 
   programs.home-manager.enable = true;
