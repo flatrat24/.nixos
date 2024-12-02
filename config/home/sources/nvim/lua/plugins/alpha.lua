8b310980b1135f0d9951687b62d35be2693e3301
Ethan Anthony <ethan.anthony@du.edu>
2024-12-02 18:04:20 -0500

    removed unused alpha.nvim optiosn

--- config/home/sources/nvim/lua/plugins/alpha.lua
+++ config/home/sources/nvim/lua/plugins/alpha.lua
@@ -19,14 +19,14 @@ return {
       [[                                                    ]],
     }
     dashboard.section.buttons.val = {
-      dashboard.button("n", "  New file", "<cmd>ene<CR>"),
+      -- dashboard.button("n", "  New file", "<cmd>ene<CR>"),
       dashboard.button("d", "  File Manager", "<cmd>cd ~/ | Yazi<CR>"),
       dashboard.button("f", "󰈞  Find file", "<cmd>cd ~/ | Telescope find_files<CR>"),
       dashboard.button("g", "󰊄  Live grep", "<cmd>cd ~/Documents | Telescope live_grep<CR>"),
-      dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
-      dashboard.button("s", "  School", "<cmd>cd ~/Documents/School/ | Yazi<CR>"),
-      dashboard.button("p", "  Personal", "<cmd>cd ~/Documents/Personal/ | Yazi<CR>"),
-      dashboard.button("c", "  Configuration", "<cmd>cd ~/.dotfiles/ | Yazi<CR>"),
+      -- dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
+      -- dashboard.button("s", "  School", "<cmd>cd ~/Documents/School/ | Yazi<CR>"),
+      -- dashboard.button("p", "  Personal", "<cmd>cd ~/Documents/Personal/ | Yazi<CR>"),
+      -- dashboard.button("c", "  Configuration", "<cmd>cd ~/.dotfiles/ | Yazi<CR>"),
       dashboard.button("u", "  Update plugins", "<cmd>Lazy sync<CR>"),
       dashboard.button("q", "󰅚  Quit", "<cmd>qa<CR>"),
     }
