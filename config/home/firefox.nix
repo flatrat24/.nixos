5ee49b94d01487ca7846ec50f976418fb681159f
Ethan Anthony <ethan.anthony@du.edu>
2024-12-01 22:48:42 -0500

    reformatted xdg

--- config/home/firefox.nix
+++ config/home/firefox.nix
@@ -113,10 +113,14 @@ in {
     };
   };
 
-  xdg.mimeApps.defaultApplications = {
-    "text/html" = ["firefox.desktop"];
-    "text/xml" = ["firefox.desktop"];
-    "x-scheme-handler/http" = ["firefox.desktop"];
-    "x-scheme-handler/https" = ["firefox.desktop"];
+  xdg = {
+    enable = true;
+    mimeApps.enable = true;
+    mimeApps.defaultApplications = {
+      "text/html" = ["firefox.desktop"];
+      "text/xml" = ["firefox.desktop"];
+      "x-scheme-handler/http" = ["firefox.desktop"];
+      "x-scheme-handler/https" = ["firefox.desktop"];
+    };
   };
 }
