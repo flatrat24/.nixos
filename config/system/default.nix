e762dee58b6e88462bf8eb9b23089d26af8b304f
Ethan Anthony <ethan.anthony@du.edu>
2024-12-03 02:46:03 -0500

    bundle all system modules

---
+++ config/system/default.nix
@@ -0,0 +1,19 @@
+{ pkgs, lib, config, ... }: {
+  imports = [
+    ./modules/basicPackages.nix
+    ./modules/basicSettings.nix
+    ./modules/bluetooth.nix
+    ./modules/default.nix
+    ./modules/grub.nix
+    ./modules/hyprland.nix
+    ./modules/network.nix
+    ./modules/nvidia.nix
+    ./modules/pipewire.nix
+    ./modules/power.nix
+    ./modules/printing.nix
+    ./modules/sddm.nix
+    ./modules/stylix.nix
+    ./modules/syncthing.nix
+    ./modules/system76.nix
+  ];
+}
