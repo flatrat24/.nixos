bd3f6b1abc931ac1eb4130524546f532fc9b4312
Ethan Anthony <ethan.anthony@du.edu>
2024-12-03 21:02:26 -0500

    modularized monitors

--- config/home/modules/hypr/monitors/default.nix
+++ config/home/modules/hypr/monitors/default.nix
@@ -1,15 +1,21 @@
-{ inputs, pkgs, ... }:
-let
-  dependencies = with pkgs; [ ];
-in {
+{ pkgs, lib, inputs, config, ... }: {
   imports = [
-    # ./framework.nix
-    ./davidsMonitor.nix
+    ./framework.nix
+    ./microstepMSI.nix
   ];
 
-  wayland.windowManager.hyprland.settings = {
-    "monitor" = [
-      ", preferred, auto-right, 1"
-    ];
+  options = {
+    monitors.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = config.hypr.enable;
+    };
+  };
+
+  config = lib.mkIf config.monitors.enable {
+    wayland.windowManager.hyprland.settings = {
+      "monitor" = [
+        ", preferred, auto-right, 1"
+      ];
+    };
   };
 }
