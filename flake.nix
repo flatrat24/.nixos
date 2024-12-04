e8abc94027d887fc1c4e2fb4fa42416381bddc57
Ethan Anthony <ethan.anthony@du.edu>
2024-12-04 06:55:01 -0500

    removed old stylix input

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # stylix.url = "github:danth/stylix/cf8b6e2d4e8aca8ef14b839a906ab5eb98b08561";
     stylix.url = "github:danth/stylix/release-24.11";
     textfox.url = "github:adriankarlen/textfox";
     firefox-addons = {
