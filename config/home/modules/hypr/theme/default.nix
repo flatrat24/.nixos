8ddf559f0231eff60513e8cfe915d4b36844e1d0
Ethan Anthony <ethan.anthony@du.edu>
2024-12-03 21:02:31 -0500

    modularized theme

--- config/home/modules/hypr/theme/default.nix
+++ config/home/modules/hypr/theme/default.nix
@@ -1,64 +1,76 @@
-{ inputs, pkgs, ... }:
-
-{
-  wayland.windowManager.hyprland.settings = {
-    general = {
-      "gaps_in" = "5";
-      "gaps_out" = "5";
-      "border_size" = "3";
-      "resize_on_border" = "true" ;
-      "allow_tearing" = "false";
-      "layout" = "master";
-    };
-
-    decoration = {
-      "rounding" = "0";
-      "active_opacity" = "1.0";
-      "inactive_opacity" = "1.0";
-      "drop_shadow" = "true";
-      "shadow_range" = "4";
-      "shadow_render_power" = "3";
-      "blur" = {
-        "enabled" = "true";
-        "size" = "3";
-        "passes" = "1";
-        "vibrancy" = "0.1696";
-      };
-    };
-
-    animations = {
-      "enabled" = "true";
-      "bezier" = "myBezier, 0.05, 0.9, 0.1, 1.05";
-      "animation" = [
-        "windows, 1, 2, myBezier, slidein"
-        "border, 1, 2, default"
-        "fade, 1, 2, default"
-        "workspaces, 1, 2, default, slide"
-      ];
-    };
-
-    dwindle = {
-      "pseudotile" = "true";
-      "preserve_split" = "true";
-    };
-
-    master = {
-      "new_status" = "slave";
-    };
-
-    misc = {
-      "force_default_wallpaper" = "-1";
-      "disable_hyprland_logo" = "true";
-    };
-
-    group = {
-      groupbar = {
-        "enabled" = "true";
-        "font_size" = "10";
-        "font_family" = "IBM Plex Sans";
-        "height" = "14";
-        "render_titles" = "true";
-      };
+{ pkgs, lib, inputs, config, ... }: {
+  options = {
+    hypr.animations.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
     };
   };
+
+  config = lib.mkMerge [
+    (lib.mkIf (config.hypr.enable == true) {
+      wayland.windowManager.hyprland.settings = {
+        general = {
+          "gaps_in" = "5";
+          "gaps_out" = "5";
+          "border_size" = "3";
+          "resize_on_border" = "true" ;
+          "allow_tearing" = "false";
+          "layout" = "master";
+        };
+
+        decoration = {
+          "rounding" = "0";
+          "active_opacity" = "1.0";
+          "inactive_opacity" = "1.0";
+          "drop_shadow" = "true";
+          "shadow_range" = "4";
+          "shadow_render_power" = "3";
+          "blur" = {
+            "enabled" = "true";
+            "size" = "3";
+            "passes" = "1";
+            "vibrancy" = "0.1696";
+          };
+        };
+
+        animations = lib.mkMerge [
+          (lib.mkIf (config.hypr.animations.enable == true) { "enabled" = "true"; })
+          (lib.mkIf (config.hypr.animations.enable == false) { "enabled" = "false"; })
+          { # Unconditional
+            "bezier" = "myBezier, 0.05, 0.9, 0.1, 1.05";
+            "animation" = [
+              "windows, 1, 2, myBezier, slidein"
+              "border, 1, 2, default"
+              "fade, 1, 2, default"
+              "workspaces, 1, 2, default, slide"
+            ];
+          }
+        ];
+
+        dwindle = {
+          "pseudotile" = "true";
+          "preserve_split" = "true";
+        };
+
+        master = {
+          "new_status" = "slave";
+        };
+
+        misc = {
+          "force_default_wallpaper" = "-1";
+          "disable_hyprland_logo" = "true";
+        };
+
+        group = {
+          groupbar = {
+            "enabled" = "true";
+            "font_size" = "10";
+            "font_family" = "IBM Plex Sans";
+            "height" = "14";
+            "render_titles" = "true";
+          };
+        };
+      };
+    })
+  ];
 }
