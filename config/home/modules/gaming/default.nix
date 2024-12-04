af073129c4e2ac8b3cc9dccf3a111a3bcf9754b0
Ethan Anthony <ethan.anthony@du.edu>
2024-12-04 02:49:26 -0500

    solid gaming module

--- config/home/modules/gaming/default.nix
+++ config/home/modules/gaming/default.nix
@@ -1,6 +1,12 @@
 # TODO: Make this modular, I don't want to do this right now
 
-{ pkgs, lib, config, ... }: {
+{ pkgs, lib, config, ... }:
+let
+  dependencies = with pkgs; [
+    mangohud
+    protonup
+  ];
+in {
   options = {
     gaming.enable = lib.mkEnableOption "enables gaming";
   };
@@ -12,6 +18,16 @@
 
       # Mindustry
       mindustry-wayland
-    ];
+
+      # Launchers
+      lutris
+      heroic
+      bottles
+    ] ++ dependencies;
+
+    home.sessionVariables = {
+      STEAM_EXTRA_COMPAT_TOOLS_PATHS = 
+        "\${HOME}/.steam/room/compatibilitytools.d";
+    };
   };
 }
