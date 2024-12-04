5a2b2029a334d3924a081589f1ea05c377a398bf
Ethan Anthony <ethan.anthony@du.edu>
2024-12-03 21:02:20 -0500

    modularized keybinds

--- config/home/modules/hypr/keybinds/launchApplications.nix
+++ config/home/modules/hypr/keybinds/launchApplications.nix
@@ -1,10 +1,14 @@
-{ inputs, pkgs, ... }: {
-  wayland.windowManager.hyprland.settings = {
-    bindd = [
-      ##### Launch Applications #####
-      "$mod, Escape, Lock Screen, exec, hyprlock"
-      "$mod, A, Launch Terminal, exec, $terminal"
-      "$mod, S, Launch Browser, exec, $browser"
-    ];
+{ pkgs, lib, inputs, config, ... }: {
+  options = {};
+
+  config = lib.mkIf config.hypr.enable {
+    wayland.windowManager.hyprland.settings = {
+      bindd = [
+        ##### Launch Applications #####
+        "$mod, Escape, Lock Screen, exec, hyprlock"
+        "$mod, A, Launch Terminal, exec, $terminal"
+        "$mod, S, Launch Browser, exec, $browser"
+      ];
+    };
   };
 }
