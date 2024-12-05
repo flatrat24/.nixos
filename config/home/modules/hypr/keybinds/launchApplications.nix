b5292e9955dd631d9e7e95f5a7b517d0cd948f63
Ethan Anthony <ethan.anthony@du.edu>
2024-12-05 00:28:49 -0500

    added wofi modularity

--- config/home/modules/hypr/keybinds/launchApplications.nix
+++ config/home/modules/hypr/keybinds/launchApplications.nix
@@ -1,14 +1,42 @@
 { pkgs, lib, inputs, config, ... }: {
   options = {};
 
-  config = lib.mkIf config.hypr.enable {
-    wayland.windowManager.hyprland.settings = {
-      bindd = [
-        ##### Launch Applications #####
-        "$mod, Escape, Lock Screen, exec, hyprlock"
-        "$mod, A, Launch Terminal, exec, $terminal"
-        "$mod, S, Launch Browser, exec, $browser"
-      ];
-    };
-  };
+  # config = lib.mkIf config.hypr.enable {
+  #   wayland.windowManager.hyprland.settings = {
+  #     bindd = [
+  #       ##### Launch Applications #####
+  #       "$mod, Escape, Lock Screen, exec, hyprlock"
+  #       "$mod, A, Launch Terminal, exec, $terminal"
+  #       "$mod, S, Launch Browser, exec, $browser"
+  #     ];
+  #   };
+  # };
+
+  config = lib.mkIf config.hypr.enable (lib.mkMerge [
+    {
+      wayland.windowManager.hyprland.settings = {
+        bindd = [
+          "$mod, Escape, Lock Screen, exec, hyprlock"
+          "$mod, A, Launch Terminal, exec, $terminal"
+          "$mod, S, Launch Browser, exec, $browser"
+        ];
+      };
+    }
+    (lib.mkIf config.wofi.enable (lib.mkMerge [
+      {
+        wayland.windowManager.hyprland.settings = {
+          bindd = [
+            "$mod, Space, Wofi Drun, exec, wofi -c ~/.config/wofi/configs/default --show drun"
+          ];
+        };
+      }
+      (lib.mkIf config.wofi.emoji.enable {
+        wayland.windowManager.hyprland.settings = {
+          bindd = [
+            "$mod, e, Wofi Emoji, exec, wofi-emoji"
+          ];
+        };
+      })
+    ]))
+  ]);
 }
