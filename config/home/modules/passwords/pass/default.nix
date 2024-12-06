d1d8d75bab5721445e281d1e346c42dc749cb0ac
Ethan Anthony <ethan.anthony@du.edu>
2024-12-06 03:30:50 -0500

    stopping point, neet to get stuff working tho

--- config/home/modules/passwords/pass/default.nix
+++ config/home/modules/passwords/pass/default.nix
@@ -1,3 +1,11 @@
+# Kinda works, but I don't fully know
+# I still need to get:
+#   - KeepassXC database imported
+#   - Initialized as a git repository (i think is safe with gpg)
+#   - Reliably duplicated on multiple machines
+#     - GPG duplicated
+#     - Synced with Git (not with syncthing)
+
 { pkgs, lib, config, ... }:
 let
   dependencies = with pkgs; [ ];
@@ -13,5 +21,15 @@ in {
 
   config = lib.mkIf config.passwords.pass.enable {
     home.packages = dependencies;
+
+    programs = {
+      password-store = {
+        enable = true;
+        package = pkgs.pass.withExtensions (exts: [exts.pass-import]);
+        settings = {
+          PASSWORD_STORE_DIR = "${config.home.homeDirectory}/.password-store";
+        };
+      };
+    };
   };
 }
