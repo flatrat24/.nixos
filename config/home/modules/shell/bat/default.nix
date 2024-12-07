80161bfc3d24795b9ab4ab5ea71d634941691415
Ethan Anthony <ethan.anthony@du.edu>
2024-12-06 19:19:59 -0500

    added bat module for shell

---
+++ config/home/modules/shell/bat/default.nix
@@ -0,0 +1,46 @@
+{ pkgs, lib, config, ... }:
+let
+  dependencies = with pkgs; [
+    bat
+  ];
+  batAliases = {
+    "cat" = "bat";
+  };
+in {
+  options = {
+    shell.programs.bat = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = config.shell.enable;
+      };
+      bash.enable = lib.mkOption {
+        type = lib.types.bool;
+        default = (config.shell.programs.bat.enable && config.shell.bash.enable);
+      };
+      zsh.enable = lib.mkOption {
+        type = lib.types.bool;
+        default = (config.shell.programs.bat.enable && config.shell.zsh.enable);
+      };
+    };
+  };
+
+  config = lib.mkIf config.shell.programs.bat.enable (lib.mkMerge [
+    {
+      home.packages = dependencies;
+
+      home.file = {
+        ".config/bat" = {
+          source = ./sources/;
+          executable = true;
+          recursive = false;
+        };
+      };
+    }
+    (lib.mkIf config.shell.programs.bat.bash.enable {
+      programs.bash.shellAliases = batAliases;
+    })
+    (lib.mkIf config.shell.programs.bat.zsh.enable {
+      programs.zsh.shellAliases = batAliases;
+    })
+  ]);
+}
