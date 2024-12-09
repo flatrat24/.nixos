01e2128838e1531dd4ad53fbfb1c6249149d7195
Ethan Anthony <ethan.anthony@du.edu>
2024-12-08 19:37:11 -0500

    added zsh and bash integration conditionalyl

--- config/home/modules/gpg/default.nix
+++ config/home/modules/gpg/default.nix
@@ -8,22 +8,30 @@
   };
 
 
-  config = lib.mkIf config.gpg.enable {
-    programs = {
-      gpg = {
-        enable = true;
-        homedir = "${config.home.homeDirectory}/.gnupg";
+  config = lib.mkIf config.gpg.enable (lib.mkMerge [
+    {
+      programs = {
+        gpg = {
+          enable = true;
+          homedir = "${config.home.homeDirectory}/.gnupg";
+        };
       };
-    };
 
-    services = {
-      gpg-agent = {
-        enable = true;
-        enableSshSupport = true;
-        pinentryPackage = pkgs.pinentry-curses;
-        maxCacheTtl = 7 * 24 * 60 * 60; # One week
-        defaultCacheTtl = 7 * 24 * 60 * 60; # One week
+      services = {
+        gpg-agent = {
+          enable = true;
+          enableSshSupport = true;
+          pinentryPackage = pkgs.pinentry-curses;
+          maxCacheTtl = 7 * 24 * 60 * 60; # One week
+          defaultCacheTtl = 7 * 24 * 60 * 60; # One week
+        };
       };
-    };
-  };
+    }
+    (lib.mkIf config.shell.zsh.enable {
+      services.gpg-agent.enableZshIntegration = true;
+    })
+    (lib.mkIf config.shell.bash.enable {
+      services.gpg-agent.enableBashIntegration = true;
+    })
+  ]);
 }
