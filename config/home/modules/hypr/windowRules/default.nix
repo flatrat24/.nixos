3e326dbf2eacb16e2de48bd8dfbf3db989a53376
Ethan Anthony <ethan.anthony@du.edu>
2024-12-14 23:57:55 -0500

    refactored windowrules for hyprland

--- config/home/modules/hypr/windowRules/default.nix
+++ config/home/modules/hypr/windowRules/default.nix
@@ -1,11 +1,7 @@
 { pkgs, lib, inputs, config, ... }: {
   options = {
     hypr.windowRules = {
-      preventAutoFullscreen = lib.mkOption {
-        type = lib.types.bool;
-        default = config.hypr.enable;
-      };
-      battlenet = lib.mkOption { # Make dependent on gaming (is this too much modularity)
+      enable = lib.mkOption {
         type = lib.types.bool;
         default = config.hypr.enable;
       };
@@ -13,55 +9,39 @@
   };
   
   config = lib.mkMerge [
-    (lib.mkIf (config.hypr.windowRules.preventAutoFullscreen == true) {
+    (lib.mkIf (config.hypr.windowRules.enable) {
       wayland.windowManager.hyprland.settings.windowrulev2 = [
         "suppressevent maximize, class:.*"
       ];
     })
-    (lib.mkIf (config.hypr.windowRules.battlenet == true) {
+    (lib.mkIf (config.hypr.windowRules.enable && config.gaming.enable) {
       wayland.windowManager.hyprland.settings.windowrulev2 = [
+        ##--- Minecraft ---##
+        "float,class:Minecraft*,title:Minecraft*"
+        "size 75% 75%,class:Minecraft*,title:Minecraft*"
+        "move 12.5% 12.5%,class:Minecraft*,title:Minecraft*"
+
+        ##--- Battle.net ---##
         # Login Popup
         "float,class:battle.net.exe,title:Battle.net Login"
-        # "noborder,class:battle.net.exe,title:Battle.net Login"
         "size 50% 50%,class:battle.net.exe,title:Battle.net Login"
         "move 25% 25%,class:battle.net.exe,title:Battle.net Login"
-
+        # Battle.net client
         "float,class:battle.net.exe,title:Battle.net"
-        # "noborder,class:battle.net.exe,title:Battle.net"
         "size 75% 75%,class:battle.net.exe,title:Battle.net"
         "move 12.5% 12.5%,class:battle.net.exe,title:Battle.net"
-
         # Little weird system tray thing
         # "float,class:explorer.exe,title:Wine System Tray"
         # "size 40% 40%,class:explorer.exe,title:Wine System Tray"
         # "move 30% 30%,class:explorer.exe,title:Wine System Tray"
       ];
     })
-    # wayland.windowManager.hyprland.settings = {
-    #   windowrulev2 = [
-    #     ##### Prevent auto-maximized applications #####
-    #     "suppressevent maximize, class:.*"
-    #
-    #     # ##### Calculator #####
-    #     # "size 20% 20%,title:Calculator"
-    #     # "float,title:Calculator"
-    #     # "move 40% 40%,title:Calculator"
-    #
-    #     # ##### System Monitor #####
-    #     # "size 75% 75%,title:SystemMonitor"
-    #     # "float,title:SystemMonitor"
-    #     # "move 12.5% 12.5%,title:SystemMonitor"
-    #
-    #     # ##### KeePassXC Browswer Extension Popup #####
-    #     # "float,class:org.keepassxc.KeePassXC,title:Unlock Database - KeePassXC"
-    #     # "size 40% 40%,class:org.keepassxc.KeePassXC,title:Unlock Database - KeePassXC"
-    #     # "move 30% 30%,class:org.keepassxc.KeePassXC,title:Unlock Database - KeePassXC"
-    #
-    #     # ##### Thunderbird Popups #####
-    #     # "float,class:thunderbird,title:Edit Item"
-    #     # "size 50% 50%,class:thunderbird,title:Edit Item"
-    #     # "move 25% 25%,class:thunderbird,title:Edit Item"
-    #   ];
-    # };
+    (lib.mkIf (config.hypr.windowRules.enable && config.firefox.enable) {
+      wayland.windowManager.hyprland.settings.windowrulev2 = [
+        "float,class:firefox,title:Picture-in-Picture"
+        "size 50% 50%,class:firefox,title:Picture-in-Picture"
+        "move 25% 25%,class:firefox,title:Picture-in-Picture"
+      ];
+    })
   ];
 }
