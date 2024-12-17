f2b87df1e9080b4a551cb395058fccbf2c821636
Ethan Anthony <ethan.anthony@du.edu>
2024-12-17 18:10:43 -0500

    moved conditionaly windowrules to relevant modules

--- config/home/modules/hypr/windowRules/default.nix
+++ config/home/modules/hypr/windowRules/default.nix
@@ -7,41 +7,12 @@
       };
     };
   };
-  
-  config = lib.mkMerge [
-    (lib.mkIf (config.hypr.windowRules.enable) {
+
+  config = lib.mkIf (config.hypr.windowRules.enable == true) (lib.mkMerge [
+    {
       wayland.windowManager.hyprland.settings.windowrulev2 = [
         "suppressevent maximize, class:.*"
       ];
-    })
-    (lib.mkIf (config.hypr.windowRules.enable && config.gaming.enable) {
-      wayland.windowManager.hyprland.settings.windowrulev2 = [
-        ##--- Minecraft ---##
-        "float,class:Minecraft*,title:Minecraft*"
-        "size 75% 75%,class:Minecraft*,title:Minecraft*"
-        "move 12.5% 12.5%,class:Minecraft*,title:Minecraft*"
-
-        ##--- Battle.net ---##
-        # Login Popup
-        "float,class:battle.net.exe,title:Battle.net Login"
-        "size 50% 50%,class:battle.net.exe,title:Battle.net Login"
-        "move 25% 25%,class:battle.net.exe,title:Battle.net Login"
-        # Battle.net client
-        "float,class:battle.net.exe,title:Battle.net"
-        "size 75% 75%,class:battle.net.exe,title:Battle.net"
-        "move 12.5% 12.5%,class:battle.net.exe,title:Battle.net"
-        # Little weird system tray thing
-        # "float,class:explorer.exe,title:Wine System Tray"
-        # "size 40% 40%,class:explorer.exe,title:Wine System Tray"
-        # "move 30% 30%,class:explorer.exe,title:Wine System Tray"
-      ];
-    })
-    (lib.mkIf (config.hypr.windowRules.enable && config.firefox.enable) {
-      wayland.windowManager.hyprland.settings.windowrulev2 = [
-        "float,class:firefox,title:Picture-in-Picture"
-        "size 50% 50%,class:firefox,title:Picture-in-Picture"
-        "move 25% 25%,class:firefox,title:Picture-in-Picture"
-      ];
-    })
-  ];
+    }    
+  ]);
 }
