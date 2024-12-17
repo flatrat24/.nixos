98630f9199b9a202e84981f828d7af4f831bc056
Ethan Anthony <ethan.anthony@du.edu>
2024-12-17 18:10:58 -0500

    first commit

---
+++ config/home/modules/neovim/plugins/rainbow-delimiters/default.nix
@@ -0,0 +1,32 @@
+{ pkgs, lib, config, ... }: {
+  options = {
+    neovim.plugins.rainbow-delimiters = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = config.neovim.enable;
+      };
+    };
+  };
+
+  config = lib.mkIf config.neovim.plugins.rainbow-delimiters.enable (lib.mkMerge [
+    {
+      programs.nixvim = {
+        plugins.rainbow-delimiters = {
+          enable = true;
+        };
+        # TODO: Make this linked to the current theme type
+        extraConfigLua = ''
+          vim.cmd[[
+            highlight RainbowDelimiterRed    guifg=#F38BA8 ctermfg=White
+            highlight RainbowDelimiterYellow guifg=#F9E2AF ctermfg=White
+            highlight RainbowDelimiterBlue   guifg=#89B4FA ctermfg=White
+            highlight RainbowDelimiterOrange guifg=#FAB387 ctermfg=White
+            highlight RainbowDelimiterGreen  guifg=#A6E3A1 ctermfg=White
+            highlight RainbowDelimiterViolet guifg=#B4BEFE ctermfg=White
+            highlight RainbowDelimiterCyan   guifg=#94E2D5 ctermfg=White
+          ]]
+        '';
+      };
+    }
+  ]);
+}
