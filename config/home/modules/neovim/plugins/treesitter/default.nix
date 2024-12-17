ee8bcc70476c94d8ccc14ed6176e1723e21ee08e
Ethan Anthony <ethan.anthony@du.edu>
2024-12-17 18:11:03 -0500

    first commit

--- config/home/modules/neovim/plugins/treesitter/default.nix
+++ config/home/modules/neovim/plugins/treesitter/default.nix
@@ -14,26 +14,7 @@
         plugins.treesitter = {
           enable = true;
 
-          grammarPackages = with pkgs.vimPlugins.nvim-treesitter-parsers; [
-            bash
-            c
-            cpp
-            css
-            html
-            hyprlang
-            json
-            kdl
-            lua
-            markdown
-            markdown_inline
-            nix
-            python
-            query
-            rasi
-            vim
-            vimdoc
-            yaml
-          ];
+          grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
 
           settings = {
             auto_install = false;
