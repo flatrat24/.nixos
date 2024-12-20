c601b923a078a5269e7832d027150feb460904ab
Ethan Anthony <ethan.anthony@du.edu>
2024-12-19 19:31:50 -0500

    added waybar back

--- config/home/modules/waybar/default.nix
+++ config/home/modules/waybar/default.nix
@@ -1,20 +1,33 @@
 { pkgs, lib, config, ... }:
 let
-  dependencies = with pkgs; [ waybar ];
+  dependencies = with pkgs; [
+    waybar
+  ];
 in {
   options = {
     waybar.enable = lib.mkEnableOption "enables waybar";
   };
 
-  config = lib.mkIf config.waybar.enable {
-    home.packages = dependencies;
+  config = lib.mkIf config.waybar.enable (lib.mkMerge [
+    {
+      home.packages = dependencies;
 
-    home.file = {
-      ".config/waybar" = {
-        source = ./sources;
-        executable = false;
-        recursive = true;
+      home.file = {
+        ".config/waybar" = {
+          source = ./sources;
+          executable = true;
+          recursive = true;
+        };
       };
-    };
-  };
+    }
+    (lib.mkIf config.hypr.enable {
+      wayland.windowManager.hyprland = {
+        settings = {
+          "exec-once" = [
+            "waybar"
+          ];
+        };
+      };
+    })
+  ]);
 }
