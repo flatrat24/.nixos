b2233a3e621e02add54c0dcc1203fd1b2654c0b6
Ethan Anthony <ethan.anthony@du.edu>
2024-12-19 20:55:41 -0500

    reworked a few keybinds

--- config/home/modules/neovim/plugins/telescope/default.nix
+++ config/home/modules/neovim/plugins/telescope/default.nix
@@ -53,8 +53,8 @@ in {
           { mode = [ "n" ]; key = "<leader>ff"; action = "<cmd>Telescope find_files<CR>"; options = { noremap = true; silent = true; desc = "Find files"; }; }
           { mode = [ "n" ]; key = "<leader>fg"; action = "<cmd>Telescope live_grep<CR>"; options = { noremap = true; silent = true; desc = "Live grep"; }; }
           { mode = [ "n" ]; key = "<leader>fb"; action = "<cmd>Telescope buffers<CR>"; options = { noremap = true; silent = true; desc = "Buffers"; }; }
-          { mode = [ "n" ]; key = "<leader>fc"; action = "<cmd>Telescope command_history<CR>"; options = { noremap = true; silent = true; desc = "Command history"; }; }
-          { mode = [ "n" ]; key = "<leader>fh"; action = "<cmd>Telescope help_tags<CR>"; options = { noremap = true; silent = true; desc = "Help"; }; }
+          { mode = [ "n" ]; key = "<leader>c"; action = "<cmd>Telescope command_history<CR>"; options = { noremap = true; silent = true; desc = "Command history"; }; }
+          { mode = [ "n" ]; key = "<leader>h"; action = "<cmd>Telescope help_tags<CR>"; options = { noremap = true; silent = true; desc = "Help"; }; }
         ];
       };
     }
