0bd7c1df332ca8189d7d238f338a8100ecb761b0
Ethan Anthony <ethan.anthony@du.edu>
2024-12-21 00:20:52 -0500

    added grug-far keybings

--- config/home/modules/neovim/plugins/grug-far/default.nix
+++ config/home/modules/neovim/plugins/grug-far/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }: {
+{ pkgs, lib, config, ... }: {
   options = {
     neovim.plugins.grug-far = {
       enable = lib.mkOption {
@@ -10,9 +10,39 @@
 
   config = lib.mkIf config.neovim.plugins.grug-far.enable {
     programs.nixvim = {
-      plugins.grug-far = {
-        enable = true;
-      };
+      extraPlugins = with pkgs.vimPlugins; [
+        grug-far-nvim
+      ];
+      extraConfigLua = ''
+        require('grug-far').setup({
+          keymaps = {
+            replace = { n = '<localleader>r' },
+            qflist = { n = '<localleader>q' },
+            syncLocations = { n = '<localleader>s' },
+            syncLine = { n = '<localleader>l' },
+            close = { n = '<localleader>c' },
+            historyOpen = { n = '<localleader>t' },
+            historyAdd = { n = '<localleader>a' },
+            refresh = { n = '<localleader>f' },
+            openLocation = { n = '<localleader>o' },
+            openNextLocation = { n = '<down>' },
+            openPrevLocation = { n = '<up>' },
+            gotoLocation = { n = '<enter>' },
+            pickHistoryEntry = { n = '<enter>' },
+            abort = { n = '<localleader>b' },
+            help = { n = 'g?' },
+            toggleShowCommand = { n = '<localleader>p' },
+            swapEngine = { n = '<localleader>e' },
+            previewLocation = { n = '<localleader>i' },
+            swapReplacementInterpreter = { n = '<localleader>x' },
+            applyNext = { n = '<localleader>j' },
+            applyPrev = { n = '<localleader>k' },
+          },
+        })
+      '';
+      keymaps = [
+        { mode = [ "n" ]; key = "<leader>vg"; action = "<cmd>GrugFar<CR>"; options = { noremap = true; silent = true; desc = "View Find and Replace"; }; }
+      ];
     };
   };
 }
