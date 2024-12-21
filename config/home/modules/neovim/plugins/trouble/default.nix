ac646c85dfe5f1dbfac9e445520a682c93accea7
Ethan Anthony <ethan.anthony@du.edu>
2024-12-21 00:24:50 -0500

    added keybinds and themeing for trouble

--- config/home/modules/neovim/plugins/trouble/default.nix
+++ config/home/modules/neovim/plugins/trouble/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, config, ... }: {
+{ lib, config, ... }: {
   options = {
     neovim.plugins.trouble = {
       enable = lib.mkOption {
@@ -14,7 +14,21 @@
         plugins.trouble = {
           enable = true;
         };
+        keymaps = [
+          { mode = [ "n" ]; key = "<leader>vd"; action = "<cmd>Trouble diagnostics<CR>"; options = { noremap = true; silent = true; desc = "View Diagnostics"; }; }
+        ];
       };
     }
+    (lib.mkIf config.neovim.plugins.todo-comments.enable {
+      programs.nixvim.keymaps = [
+        { mode = [ "n" ]; key = "<leader>vt"; action = "<cmd>TodoTrouble<CR>"; options = { noremap = true; silent = true; desc = "View Todo Comments"; }; }
+      ];
+    })
+    (lib.mkIf config.neovim.plugins.transparent.enable {
+      programs.nixvim.extraConfigLua = ''
+        vim.api.nvim_set_hl(0, "TroubleNormal", { bg = "NONE", ctermbg = "NONE" })
+        vim.api.nvim_set_hl(0, "TroubleNormalNC", { bg = "NONE", ctermbg = "NONE" })
+      '';
+    })
   ]);
 }
