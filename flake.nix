23380bc958987831f92da803c25d358072f8d822
Ethan Anthony <ethan.anthony@du.edu>
2024-12-23 02:06:50 -0500

    formatting and unifying flake inputs
    make sure flake inputs follow eachother (follow nixpkgs and hyprland)

--- flake.nix
+++ flake.nix
@@ -2,31 +2,30 @@
   description = "NixOS System Flake";
 
   inputs = {
+    ##--- follow nixpkgs ---##
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    stylix.url = "github:danth/stylix/release-24.11";
-
-    textfox.url = "github:adriankarlen/textfox";
-
+    stylix = {
+      url = "github:danth/stylix/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    ags.url = "github:Aylur/ags";
 
+    ##--- others ---##
+    textfox.url = "github:adriankarlen/textfox";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    ags.url = "github:Aylur/ags";
     minegrub-theme.url = "github:Lxtharia/minegrub-theme";
   };
 
