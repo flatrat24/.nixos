2d2aac87bcfc4527bbfb92199f64b2efad630cca
Ethan Anthony <ethan.anthony@du.edu>
2024-12-23 23:54:52 -0500

    better keybinds

--- config/home/modules/neovim/plugins/neo-tree/default.nix
+++ config/home/modules/neovim/plugins/neo-tree/default.nix
@@ -13,6 +13,39 @@
       programs.nixvim = {
         plugins.neo-tree = {
           enable = true;
+          filesystem = {
+            filteredItems = {
+              visible = true;
+            };
+          };
+          window = {
+            mappings = {
+              "<2-LeftMouse>" = "open";
+              "<cr>" = "set_root";
+              l = "open";
+              h = "navigate_up";
+              "<esc>" = "revert_preview";
+              P = {
+                command = "toggle_preview";
+                config = { use_float = true; };
+              };
+              n = "open_split";
+              m = "open_vsplit";
+              t = "open_tabnew";
+              w = "open_with_window_picker";
+              C = "close_node";
+              z = "close_all_nodes";
+              R = "refresh";
+              y = "copy_to_clipboard";
+              x = "cut_to_clipboard";
+              p = "paste_from_clipboard";
+              e = "toggle_auto_expand_width";
+              q = "close_window";
+              "?" = "show_help";
+              "<" = "prev_source";
+              ">" = "next_source";
+            };
+          };
         };
         keymaps = [
           { mode = [ "n" ]; key = "<leader>ve"; action = "<cmd>Neotree toggle reveal filesystem right<CR>"; options = { noremap = true; silent = true; desc = "View Filesystem Tree"; }; }
