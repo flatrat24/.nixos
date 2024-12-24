6ee156a1926aed42c895bd9dfdf62344a2bdb5ed
Ethan Anthony <ethan.anthony@du.edu>
2024-12-24 00:32:19 -0500

    settings for undotree, moved to right

--- config/home/modules/neovim/plugins/undotree/default.nix
+++ config/home/modules/neovim/plugins/undotree/default.nix
@@ -13,6 +13,27 @@
       programs.nixvim = {
         plugins.undotree = {
           enable = true;
+          settings = {
+            CursorLine = true;
+            DiffAutoOpen = true;
+            DiffCommand = "diff";
+            DiffpanelHeight = 10;
+            HelpLine = true;
+            HighlightChangedText = true;
+            HighlightChangedWithSign = true;
+            HighlightSyntaxAdd = "DiffAdd";
+            HighlightSyntaxChange = "DiffChange";
+            HighlightSyntaxDel = "DiffDelete";
+            RelativeTimestamp = true;
+            SetFocusWhenToggle = true;
+            ShortIndicators = false;
+            SplitWidth = 40;
+            TreeNodeShape = "*";
+            TreeReturnShape = "\\";
+            TreeSplitShape = "/";
+            TreeVertShape = "|";
+            WindowLayout = 3;
+          };
         };
         keymaps = [
           { mode = [ "n" ]; key = "<leader>vu"; action = "<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>"; options = { noremap = true; silent = true; desc = "Toggle Undotree View"; }; }
