b5fd50563a8f99defa966e18347d9685d6ec0193
Ethan Anthony <ethan.anthony@du.edu>
2024-12-24 00:32:24 -0500

    got vimtex working!!!!!!!!!!!!!!!!

--- config/home/modules/neovim/plugins/vimtex/default.nix
+++ config/home/modules/neovim/plugins/vimtex/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }: {
+{ pkgs, lib, config, ... }: {
   options = {
     neovim.plugins.vimtex = {
       enable = lib.mkOption {
@@ -13,9 +13,51 @@
       programs.nixvim = {
         plugins.vimtex = {
           enable = true;
-          settings = {
-            view_method = "zathura";
-          };
+          texlivePackage = pkgs.texliveFull;
+          # settings = {
+          #   fold_enabled = true;
+          #   view_method = "zathura";
+          #   compiler_method = "latexmk";
+          #   fold_manual = true;
+          #   fold_types = {
+          #     preamble.enabled = true;
+          #     sections.enabled = true;
+          #     parts.enabled = true;
+          #     comments.enabled = false;
+          #     envs.whitelist = ["frame" "abstract"];
+          #     env_options.enabled = false;
+          #     items.enabled = false;
+          #     markers.enabled = false;
+          #     cmd_single.enabled = false;
+          #     cmd_single_opt.enabled = false;
+          #     cmd_multi.enabled = false;
+          #     cmd_addplot.enabled = false;
+          #   };
+          #   indent_enabled = false;
+          #   matchparen_enabled = false;
+          #   toc_config = {
+          #     split_pos = "vert topleft";
+          #     split_width = 40;
+          #     mode = 1;
+          #     fold_enable = true;
+          #     fold_level_start = -1;
+          #     show_help = false;
+          #     resize = false;
+          #     show_numbers = true;
+          #     layer_status = {
+          #       label = 0;
+          #       include = 0;
+          #       todo = 0;
+          #       content = 1;
+          #     };
+          #     hide_line_numbers = false;
+          #     tocdepth = 2;
+          #     indent_levels = 1;
+          #   };
+          #   toc_show_preamble = false;
+          #
+          #   doc_confirm_single = false;
+          # };
         };
         # keymaps = [
         #   { mode = [ "n" ]; key = "<leader>vu"; action = "<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>"; options = { noremap = true; silent = true; desc = "Toggle Undotree View"; }; }
