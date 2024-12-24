0596f51b7c332562d39c72629e75d44d98bd6efd
Ethan Anthony <ethan.anthony@du.edu>
2024-12-23 22:55:55 -0500

    fixed outdated homemanager

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     ##--- follow nixpkgs ---##
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";  # this selects the release-branch and needs to match Nixpkgs
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
