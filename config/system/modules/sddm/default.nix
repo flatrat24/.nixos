d38d73119b9836890822a5a4876fde3a8c9655c7
Ethan Anthony <ethan.anthony@du.edu>
2024-12-29 00:35:43 -0500

    sddm with qt6

--- config/system/modules/sddm/default.nix
+++ config/system/modules/sddm/default.nix
@@ -1,29 +1,14 @@
-{ pkgs, lib, config, ... }:
-let
-  tokyo-night-sddm = pkgs.libsForQt5.callPackage ../../../../pkgs/tokyo-night-sddm { };
-  sddm-astronaut-theme = pkgs.libsForQt5.callPackage ../../../../pkgs/sddm-astronaut-theme { };
-in {
+{ pkgs, lib, config, ... }: {
   options = {
     sddm.enable = lib.mkEnableOption "enables sddm";
   };
 
   config = lib.mkIf config.sddm.enable {
-    environment.defaultPackages = with pkgs; [
-      tokyo-night-sddm
-      sddm-astronaut
-    ];
-
     services.displayManager.sddm = {
       enable = true;
-      # defaultSession = "hyprland";
       wayland.enable = true;
+      package = pkgs.kdePackages.sddm; # qt6 sddm version
       enableHidpi = true;
-      theme = "tokyo-night-sddm";
-      settings = {
-        General = {
-          GreeterEnvironment = "QT_SCREEN_SCALE_FACTORS=2,QT_FONT_DPI=192";
-        };
-      };
     };
   };
 }
