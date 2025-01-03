41c15b31f6c75e435d27aa1c4c7aa2784976a6e0
Ethan Anthony <ethan.anthony@du.edu>
2025-01-03 10:43:56 -0700

    enabled blur but not by default

--- config/home/modules/hyprland/theme/default.nix
+++ config/home/modules/hyprland/theme/default.nix
@@ -50,6 +50,20 @@ in {
           "disable_hyprland_logo" = "true";
         };
 
+        decoration = {
+          active_opacity = "1.0";
+          inactive_opacity = "1.0";
+          fullscreen_opacity = "1.0";
+
+          blur = {
+            enabled = "true";
+            size = "8";
+            passes = "2";
+            new_optimizations = "true";
+            ignore_opacity = "true";
+          };
+        };
+
         group = {
           groupbar = {
             "enabled" = "true";
