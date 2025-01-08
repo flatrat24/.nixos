d88f308f09b45e41e66f11492161a6f5b5f191f4
Ethan Anthony <ethan.anthony@du.edu>
2025-01-08 10:34:30 -0700

    added yazi integration

--- config/home/modules/pinta/default.nix
+++ config/home/modules/pinta/default.nix
@@ -7,7 +7,16 @@ in {
     pinta.enable = lib.mkEnableOption "enable pinta";
   };
 
-  config = lib.mkIf cfg.enable {
-    home.packages = dependencies;
-  };
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    {
+      home.packages = dependencies;
+    }
+    (lib.mkIf config.yazi.enable {
+      programs.yazi.settings.opener = {
+        image = [
+          { run = ''pinta "$@"''; orphan = true; desc = " pinta"; }
+        ];
+      };
+    })
+  ]);
 }
