823d082ac984a8e742fac524e39a470c8e6643aa
Ethan Anthony <ethan.anthony@du.edu>
2025-01-23 18:16:35 -0700

    better formatting

--- config/home/modules/neovim/plugins/render-markdown/default.nix
+++ config/home/modules/neovim/plugins/render-markdown/default.nix
@@ -16,7 +16,64 @@ in {
       programs.nixvim = {
         plugins.render-markdown = {
           enable = true;
-          settings = { };
+          settings = {
+            heading = {
+              border = false;
+              icons = [
+                "󰎦 "
+                "󰎩 "
+                "󰎬 "
+                "󰎮 "
+                "󰎰 "
+                "󰎵 "
+              ];
+              position = "overlay";
+              sign = false;
+              width = "full";
+            };
+            paragraph = {
+              enabled = true;
+              left_margin = 0;
+              min_width = 0;
+            };
+            bullet = {
+              icons = [
+                " "
+                " "
+                " "
+                # " "
+                # " "
+                # " "
+              ];
+              right_pad = 1;
+            };
+            checkbox = {
+              enabled = true;
+              render_modes = false;
+              position = "inline";
+              unchecked = {
+                icon = " ";
+                highlight = "CurSearch";
+                scope_highlight = "nil";
+              };
+              checked = {
+                icon = " ";
+                highlight = "RenderMarkdownChecked";
+                scope_highlight = "nil";
+              };
+            };
+            render_modes = true;
+            signs = {
+              enabled = true;
+            };
+            indent = {
+              enabled = false;
+              render_modes = false;
+              per_level = 0;
+              skip_level = 0;
+              skip_heading = false;
+            };
+          };
         };
       };
     }
