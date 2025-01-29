58d0aeb879723233bd84072298f4bbea708e68fd
Ethan Anthony <ethan.anthony@du.edu>
2025-01-29 10:29:44 -0700

    comments

--- config/home/modules/hyprland/default.nix
+++ config/home/modules/hyprland/default.nix
@@ -47,9 +47,30 @@ in {
           windowrulev2 = [
             "suppressevent maximize, class:.*"
           ];
+          # bindd = [
+          #   "$mod, grave, Workspace Overview, hyprexpo:expo, toggle"
+          # ];
+          # plugin = {
+          #   hyprexpo = {
+          #     "columns" = "3";
+          #     "gap_size" = "5";
+          #     "bg_col" = "rgb(1E1E2E)";
+          #     "workspace_method" = "center current"; # [center/first] [workspace] e.g. first 1 or center m+1
+          #
+          #     "enable_gesture" = "true"; # laptop touchpad
+          #     "gesture_fingers" = "4";  # 3 or 4
+          #     "gesture_distance" = "300"; # how far is the "max"
+          #     "gesture_positive" = "true"; # positive = swipe down. Negative = swipe up.
+          #   };
+          # };
         };
         plugins = [
+          # inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.borders-plus-plus
+          # pkgs.hyprlandPlugins.borders-plus-plus
           # inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprexpo
+          # pkgs.hyprlandPlugins.hyprexpo
+          # inputs.hyprscroller.packages.${pkgs.stdenv.hostPlatform.system}.hyprscroller
+          # pkgs.hyprlandPlugins.hyprscroller
         ];
         extraConfig = ''
           exec-once=dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
