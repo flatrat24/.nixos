d5cde0f1c15811ed84660c9d154e3e302f59b4c7
Ethan Anthony <ethan.anthony@du.edu>
2025-03-28 15:59:05 -0600

    manual styling for cathura

--- config/home/modules/zathura/default.nix
+++ config/home/modules/zathura/default.nix
@@ -29,47 +29,47 @@ in{
           "p"      = "print";
         };
         options = {
-          # notification-error-bg   = "rgba(40,40,40,1)";       # bg
-          # notification-error-fg   = "rgba(251,73,52,1)";      # bright:red
-          # notification-warning-bg = "rgba(40,40,40,1)";       # bg
-          # notification-warning-fg = "rgba(250,189,47,1)";     # bright:yellow
-          # notification-bg         = "rgba(40,40,40,1)";       # bg
-          # notification-fg         = "rgba(184,187,38,1)";     # bright:green
-          #
-          # completion-bg           = "rgba(80,73,69,1)";       # bg2
-          # completion-fg           = "rgba(235,219,178,1)";    # fg
-          # completion-group-bg     = "rgba(60,56,54,1)";       # bg1
-          # completion-group-fg     = "rgba(146,131,116,1)";    # gray
-          # completion-highlight-bg = "rgba(131,165,152,1)";    # bright:blue
-          # completion-highlight-fg = "rgba(80,73,69,1)";       # bg2
-          #
-          # index-bg                = "rgba(80,73,69,1)";       # bg2
-          # index-fg                = "rgba(235,219,178,1)";    # fg
-          # index-active-bg         = "rgba(131,165,152,1)";    # bright:blue
-          # index-active-fg         = "rgba(80,73,69,1)";       # bg2
-          #
-          # inputbar-bg             = "rgba(40,40,40,1)";       # bg
-          # inputbar-fg             = "rgba(235,219,178,1)";    # fg
-          #
-          # statusbar-bg            = "rgba(80,73,69,1)";       # bg2
-          # statusbar-fg            = "rgba(235,219,178,1)";    # fg
-          #
-          # highlight-color         = "rgba(250,189,47,0.75)";  # bright:yellow
-          # highlight-active-color  = "rgba(254,128,25,0.75)";  # bright:orange
-          #
-          # default-bg              = "rgba(40,40,40,1)";       # bg
-          # default-fg              = "rgba(235,219,178,1)";    # fg
-          # render-loading          = true;
-          # render-loading-bg       = "rgba(40,40,40,1)";       # bg
-          # render-loading-fg       = "rgba(235,219,178,1)";    # fg
-          #
-          # recolor-lightcolor      = "rgba(40,40,40,1)";       # bg
-          # recolor-darkcolor       = "rgba(235,219,178,1)";    # fg
-          # recolor                 = false;
-          # recolor-keephue         = true;                     # keep original color
-          #
-          # page-padding            = 3;
-          # font                    = "fira code nerd font mono 12";
+          default-fg              = "rgba(205,214,244,1)";
+          default-bg              = "rgba(30,30,46,1)";
+
+          completion-bg           = "rgba(49,50,68,1)";
+          completion-fg           = "rgba(205,214,244,1)";
+          completion-highlight-bg = "rgba(87,82,104,1)";
+          completion-highlight-fg = "rgba(205,214,244,1)";
+          completion-group-bg     = "rgba(49,50,68,1)";
+          completion-group-fg     = "rgba(137,180,250,1)";
+
+          statusbar-fg            = "rgba(205,214,244,1)";
+          statusbar-bg            = "rgba(49,50,68,1)";
+
+          notification-bg         = "rgba(49,50,68,1)";
+          notification-fg         = "rgba(205,214,244,1)";
+          notification-error-bg   = "rgba(49,50,68,1)";
+          notification-error-fg   = "rgba(243,139,168,1)";
+          notification-warning-bg = "rgba(49,50,68,1)";
+          notification-warning-fg = "rgba(250,227,176,1)";
+
+          inputbar-fg             = "rgba(205,214,244,1)";
+          inputbar-bg             = "rgba(49,50,68,1)";
+
+          recolor                 = "true";
+          recolor-lightcolor      = "rgba(30,30,46,1)";
+          recolor-darkcolor       = "rgba(205,214,244,1)";
+
+          index-fg                = "rgba(205,214,244,1)";
+          index-bg                = "rgba(30,30,46,1)";
+          index-active-fg         = "rgba(205,214,244,1)";
+          index-active-bg         = "rgba(49,50,68,1)";
+
+          render-loading-bg       = "rgba(30,30,46,1)";
+          render-loading-fg       = "rgba(205,214,244,1)";
+
+          highlight-color         = "rgba(87,82,104,0.5)";
+          highlight-fg            = "rgba(245,194,231,0.5)";
+          highlight-active-color  = "rgba(245,194,231,0.5)";
+
+          page-padding            = 3;
+          font                    = "IBM Plex mono 10";
 
           selection-clipboard     = "clipboard";
         };
@@ -90,10 +90,10 @@ in{
         ];
       };
     })
-    (lib.mkIf config.theme.enable {
-      stylix.targets = {
-        zathura.enable = true;
-      };
-    })
+    # (lib.mkIf config.theme.enable {
+    #   stylix.targets = {
+    #     zathura.enable = true;
+    #   };
+    # })
   ]);
 }
