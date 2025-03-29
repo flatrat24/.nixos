6b9ea4152b3074eae938ef7ce22e48da048d7836
Ethan Anthony <ethan.anthony@du.edu>
2025-03-28 21:01:45 -0600

    added new beeper

--- config/home/modules/beeper/default.nix
+++ config/home/modules/beeper/default.nix
@@ -1,7 +1,10 @@
 { pkgs, lib, inputs, config, ... }:
 let
   cfg = config.beeper;
-  dependencies = with pkgs; [ beeper ];
+  beeper = import ../../../../derivations/beeper.nix { inherit pkgs; };
+  dependencies = [
+    beeper
+  ];
 in {
   options = {
     beeper.enable = lib.mkEnableOption "enable beeper";
@@ -15,7 +18,7 @@ in {
       wayland.windowManager.hyprland = {
         settings = {
           "exec-once" = [
-            "[workspace 9 silent] beeper"
+            "[workspace 9 silent] beepertexts"
           ];
           windowrulev2 = [
             "group set,title:Beeper,class:Beeper"
@@ -25,3 +28,39 @@ in {
     })
   ]);
 }
+
+# { pkgs, lib, inputs, config, ... }:
+# let
+#   pkgs = import (builtins.fetchGit {
+#     # Descriptive name to make the store path easier to identify
+#     name = "my-old-revision";
+#     url = "https://github.com/NixOS/nixpkgs/";
+#     ref = "refs/heads/nixos-24.05";
+#     rev = "5a83f6f984f387d47373f6f0c43b97a64e7755c0";
+#   }) {};
+#   myPkg = pkgs.beeper;
+#   cfg = config.beeper;
+#   dependencies = [ myPkg ];
+# in {
+#   options = {
+#     beeper.enable = lib.mkEnableOption "enable beeper";
+#   };
+#
+#   config = lib.mkIf cfg.enable (lib.mkMerge [
+#     {
+#       home.packages = dependencies;
+#     }
+#     (lib.mkIf config.hyprland.enable {
+#       wayland.windowManager.hyprland = {
+#         settings = {
+#           "exec-once" = [
+#             "[workspace 9 silent] beeper"
+#           ];
+#           windowrulev2 = [
+#             "group set,title:Beeper,class:Beeper"
+#           ];
+#         };
+#       };
+#     })
+#   ]);
+# }
---
+++ derivations/beeper.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }: let
+  pname = "beepertexts";
+  version = "1.0.0";
+
+  src = pkgs.fetchurl {
+    url = "https://api.beeper.com/desktop/download/linux/x64/stable/com.automattic.beeper.desktop";
+    hash = "sha256-rzFT7NfXeFt9W3DjJ0yyCzTtPSdB+FjYQHjxPbeMciU=";
+  };
+  appimageContents = pkgs.appimageTools.extract {inherit pname version src;};
+in
+  pkgs.appimageTools.wrapType2 {
+    inherit pname version src;
+    pkgs = pkgs;
+    extraInstallCommands = ''
+        install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
+        substituteInPlace $out/share/applications/${pname}.desktop \
+          --replace 'Exec=AppRun' 'Exec=${pname}'
+        cp -r ${appimageContents}/usr/share/icons $out/share
+
+        # unless linked, the binary is placed in $out/bin/cursor-someVersion
+        # ln -s $out/bin/${pname}-${version} $out/bin/${pname}
+    '';
+
+    extraBwrapArgs = [
+      "--bind-try /etc/nixos/ /etc/nixos/"
+    ];
+
+    # vscode likes to kill the parent so that the
+    # gui application isn't attached to the terminal session
+    dieWithParent = false;
+
+    extraPkgs = pkgs: with pkgs; [
+      unzip
+      autoPatchelfHook
+      asar
+      # override doesn't preserve splicing https://github.com/NixOS/nixpkgs/issues/132651
+      (buildPackages.wrapGAppsHook.override {inherit (buildPackages) makeWrapper;})
+    ];
+  }
