65ca7c748a5aaeb2fa3424e21a686799d6ad6496
Ethan Anthony <ethan.anthony@du.edu>
2025-04-10 15:03:54 -0600

    better monitors?

--- config/home/modules/hardware/default.nix
+++ config/home/modules/hardware/default.nix
@@ -24,41 +24,6 @@ in {
   config = lib.mkMerge [
     {
       wayland.windowManager.hyprland.settings = {
-        "monitor" = [ ", preferred, auto-right, 1" "Unknown-1, disable" ] # Disables (nvidia) ghost monitor
-          ++ lib.optionals (cfg.monitor == "framework") [ "eDP-1, 2880x1920@120.00Hz, 0x0, 2" "DP-1, 1920x1080@165.00Hz, 0x0, 1" ]; # TODO: Split monitors into two and create another option to set to MSI monitor
-        bindd = [ # lib.optionals (cfg.keyboard == "framework")
-          ##### Color Picker #####
-          ", F12, Activate Color Picker, exec, hyprpicker | wl-copy"
-        ];
-        binded = [
-          ##### Screenshots #####
-          '', F11, Screenshot (Select Area), exec, hyprshot --mode region --output-folder ~/Downloads''
-          ''$mod SHIFT, F11, Screenshot (Entire Screen), exec,  hyprshot --mode output --mode active --output-folder ~/Downloads''
-        ];
-        bindeld = [
-          ##### Volume Controls #####
-          ", XF86AudioMute, Toggle Volume Mute, exec, pamixer -t"
-          ", XF86AudioLowerVolume, Decrease Volume, exec, pamixer -d 2"
-          ", XF86AudioRaiseVolume, Increase Volume, exec, pamixer -i 2"
-
-          ##### Unbound Keys #####
-          # ", F4, Toggle Volume Mute, exec, pamixer -t"
-          # ", F5, Increase Volume, exec, pamixer -i 2"
-          # ", F6, Decrease Volume, exec, pamixer -d 2"
-
-          ##### Brightness #####
-          ", XF86MonBrightnessUp, Decrease Screen Brightness, exec, brightnessctl s +2%"
-          ", XF86MonBrightnessDown, Increase Screen Brightness, exec, brightnessctl s 2%-"
-
-          ##### Screenshots #####
-          ''$mod, F11, Screenshot (Focused Window), exec,  hyprshot --mode window --output-folder ~/Downloads''
-        ];
-        bindld = [
-          ##### MPD Controls #####
-          ", XF86AudioNext, Play Next Song (MPD), exec, mpc next"
-          ", XF86AudioPlay, Play/Pause Song (MPD), exec, mpc toggle"
-          ", XF86AudioPrev, Play Previous Song (MPD), exec, mpc prev"
-        ];
         input = {
           "touchpad" = {
             "disable_while_typing" = "true";
