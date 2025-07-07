98e04e92feb54fb6a4a066baefbf4b50ef195da5
Ethan Anthony <ethananthony@worldofmail.com>
2025-07-07 11:44:15 -0600

    initial aerc config with some binds, filters, and ui options

--- config/home/modules/email/default.nix
+++ config/home/modules/email/default.nix
@@ -1,7 +1,11 @@
-{ lib, config, ... }:
+{ pkgs, lib, config, ... }:
 let
   cfg = config.email;
-  dependencies = [ ];
+  dependencies = with pkgs; [
+    glow
+    w3m
+    pandoc
+  ];
 in {
   imports = [ ];
 
@@ -16,7 +20,162 @@ in {
       programs.aerc = {
         enable = true;
         extraConfig = {
-          general.unsafe-accounts-conf = true;
+          general = {
+            unsafe-accounts-conf = true;
+            pgp-provider = "gpg";
+            term = "foot";
+          };
+          "multipart-converters" = {
+            # "text/html" = "${pkgs.pandoc}/bin/pandoc -f html -t markdown --standalone | ${pkgs.glow}/bin/glow";
+            "text/html" = "w3m -dump -T text/html -o display_link_number=true";
+          };
+          filters = {
+            ".headers" = "${pkgs.aerc}/libexec/aerc/filters/colorize";
+            "text/calendar" = "${pkgs.gawk}/bin/awk -f ${pkgs.aerc}/libexec/aerc/filters/calendar";
+            # "text/html" = "${pkgs.pandoc}/bin/pandoc -f html -t markdown --standalone | ${pkgs.glow}/bin/glow";
+            "text/html" = "w3m -dump -T text/html -o display_link_number=true";
+            # "text/plain" = "${pkgs.aerc}/libexec/aerc/filters/colorize";
+            "text/plain" = "colorize";
+            "text/*" = "${pkgs.bat}/bin/bat";
+            "message/delivery-status" = "colorize";
+            "message/rfc822" = "colorize";
+            "application/pdf" = "${pkgs.zathura}/bin/zathura -";
+            "application/x-sh" = "${pkgs.bat}/bin/bat -fP -l sh";
+            "audio/*" = "${pkgs.mpv}/bin/mpv -";
+          };
+          ui = {
+            index-column = "flags:4,name<20%,subject,date>=";
+            sidebar-width = 14;
+            mouse-enabled = true;
+            new-message-bell = false;
+            spinner = ".  ,.. ,..., ..,  ., ..,...,.. ";
+            spinner-interval = "100ms";
+            auto-mark-read = false;
+            icon-encrypted = "󰯅";
+            icon-attachment = "󰏢";
+            icon-new = "*";
+            icon-old = "*";
+            icon-flagged = "󰈾";
+            icon-deleted = "󰧧";
+            icon-unencrypted = ""; # 󰒙
+            icon-signed = "󱦹";
+            icon-unknown = "?";
+            icon-invalid = "!";
+            icon-replied = "󱞧";
+            icon-forwarded = "󱞫";
+            icon-marked = "x";
+            icon-draft = "󰟷";
+          };
+        };
+        extraAccounts = {
+          purelymail = {
+            folder-map = "${pkgs.writeText "folder-map" ''
+              Inbox = INBOX
+            ''}";
+            "folders-sort" = "Inbox,Drafts,Sent,Archive,Trash,Junk";
+          };
+        };
+        extraBinds = {
+          global = {
+            "<tab>"     = ":next-tab<enter>";
+            "<backtab>" = ":next-tab<enter>";
+            "?"         = ":help keys<enter>";
+          };
+          messages = {
+            # General
+            "q" = ":quit<enter>";
+            "?" = ":help keys<enter>";
+
+            # Pane Management
+            "<C-m>"   = ":vsplit<enter>";
+            "<C-n>"   = ":hsplit<enter>";
+            "<C-S-h>" = ":hsplit -3<enter>";
+            "<C-S-j>" = ":vsplit -3<enter>";
+            "<C-S-k>" = ":vsplit +3<enter>";
+            "<C-S-l>" = ":hsplit +3<enter>";
+
+            # Actions
+            "C"   = ":compose<enter>";
+            "D"   = ":delete<enter>";
+            "A"   = ":archive flat";
+            "T"   = ":toggle-threads<enter>";
+            "|"   = ":pipe<space>";
+            "<enter>"   = ":view<enter>";
+
+            "<space>" = ":mark -t<enter>:next<enter>";
+            "v" = ":mark -v<enter>";
+
+            "cc" = ":cf<space>";
+            "ca" = ":cf Archive <enter>";
+            "cd" = ":cf Drafts <enter>";
+            "ci" = ":cf INBOX <enter>";
+            "cj" = ":cf Junk <enter>";
+            "cs" = ":cf Sent <enter>";
+            "ct" = ":cf Trash <enter>";
+
+            "zl" = ":unfold<enter>";
+            "zL" = ":unfold -a<enter>";
+            "zh" = ":fold<enter>";
+            "zH" = ":fold -a<enter>";
+            "zz" = ":fold -t<enter>";
+            "zZ" = ":fold -at<enter>";
+
+            "rr"   = ":reply<enter>";
+            "rq"   = ":reply -q<enter>";
+            "Rr"   = ":reply -a<enter>";
+            "Rq"   = ":reply -qa<enter>";
+
+            # Navigation
+            "<c-h>" = ":collapse-folder<enter>";
+            "<c-j>" = ":next-folder<enter>";
+            "<c-k>" = ":prev-folder<enter>";
+            "<c-l>" = ":expand-folder<enter>";
+            "j" = ":next<enter>";
+            "k" = ":prev<enter>";
+            "J"   = ":next 5<enter>";
+            "K"   = ":prev 5<enter>";
+            "G"   = ":select -1";
+            "gg"  = ":select 0";
+            "f"   = ":filter<space>";
+            "/"   = ":search<space>";
+            "N"   = ":prev-result<enter>";
+            "n"   = ":next-result<enter>";
+            "Esc" = ":clear<enter>";
+          };
+          view = {
+            "q" = ":close<enter>";
+            "O" = ":open<enter>";
+            "S" = ":save<space>";
+            "|" = ":pipe<space>";
+            "D" = ":move Trash<enter>";
+            "A" = ":archive flat<enter>";
+
+            "J" = ":next<enter>";
+            "K" = ":prev<enter>";
+
+            "rr" = ":reply<enter>";
+            "rq" = ":reply -q<enter>";
+            "Rq" = ":reply -qa<enter>";
+          };
+          compose = {
+            "$noinherit" = "true";
+            "$ex" = "<C-x>";
+            "<C-k>" = ":prev-field<Enter>";
+            "<C-j>" = ":next-field<Enter>";
+            "<C-h>" = ":switch-account -p<Enter>";
+            "<C-l>" = ":switch-account -n<Enter>";
+            "<tab>" = ":next-tab<Enter>";
+            "<backtab>" = ":prev-tab<Enter>";
+          };
+          "compose::review" = {
+            "y" = ":send<Enter>";
+            "n" = ":abort<Enter>";
+            "p" = ":postpone<Enter>";
+            "q" = ":choose -o d discard abort -o p postpone postpone<Enter>";
+            "e" = ":edit<Enter>";
+            "a" = ":attach<space>";
+            "d" = ":detach<space>";
+          };
         };
         stylesets = {
           default = {
